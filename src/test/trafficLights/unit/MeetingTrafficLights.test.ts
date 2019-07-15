4b5f5077c254a2c8ba05d5f0f252da626cff1d89
chasdevs <chasdeveas@gmail.com>
2019-07-15 19:50:31 -0400

    Refactor meeting logic and unit tests.

--- src/test/trafficLights/unit/MeetingTrafficLights.test.ts
+++ src/test/trafficLights/unit/MeetingTrafficLights.test.ts
@@ -1,4 +1,4 @@
-import dayjs from "dayjs";
+import dayjs, {Dayjs} from "dayjs";
 import * as td from 'testdouble'
 import container from "../../../app/inversify.config";
 import CalendarService from "../../../app/calendar/CalendarService";
@@ -6,6 +6,8 @@ import Factory from "../../tools/Factory";
 import MeetingTrafficLights from "../../../app/trafficLights/MeetingTrafficLights";
 import BulbNetwork from "../../../app/bulbNetworks/BulbNetwork";
 import Color from "../../../app/bulbNetworks/Color";
+import Room from "../../../app/calendar/Room";
+import Bulb from "../../../app/bulbNetworks/Bulb";
 
 describe('MeetingTrafficLights Unit Test', () => {
 
@@ -19,43 +21,74 @@ describe('MeetingTrafficLights Unit Test', () => {
     afterAll(container.restore);
     afterEach(td.reset);
 
-    test('syncBulbs', async () => {
+    describe('syncBulbs', () => {
 
-        const now = dayjs()
+        let bulb: Bulb, room: Room, now: Dayjs;
+        afterEach(td.reset)
+        beforeEach(() => {
+            now = dayjs()
+            bulb = Factory.dummyBulb('My Meeting Room')
+            room = Factory.dummyRoom('My Meeting Room')
+            td.when(mockNetwork.scanForBulbs()).thenResolve([bulb]);
+            td.when(mockCalendarService.rooms()).thenResolve([room]);
+        })
 
-        // Configure mocks
-        const bulbs = Factory.dummyBulbs([{label: 'meeting exists'}, {label: 'ending soon'}, {label: 'GTfo'}, {label: 'no meetings'}, {label: 'just started'}])
-        const [bulbWithMeeting, bulbEndingSoon, bulbEnded, bulbNoMeeting, bulbJustStarted] = bulbs;
-        const rooms = Factory.dummyRooms([{name: 'Meeting exists!'}, {name: 'Ending Soon...'}, {name: 'GTFO'}, {name: 'no meetings here'}, {name: 'just started'}])
-        const [roomWithMeeting, roomEndingSoon, roomEnded, roomNoMeeting, roomJustStarted] = rooms;
+        test('meeting in progress', async () => {
+            td.when(mockCalendarService.getCurrentMeetings(room)).thenResolve({
+                currentMeeting: Factory.dummyMeeting({start: now.subtract(15, 'm')})
+            })
+            await app.syncBulbs()
+            td.verify(bulb.setColor(Color.GREEN_SOFT))
+        })
 
-        td.when(mockNetwork.scanForBulbs()).thenResolve(bulbs);
-        td.when(mockCalendarService.rooms()).thenResolve(rooms);
-        td.when(mockCalendarService.getCurrentMeetings(roomWithMeeting)).thenResolve({
-            currentMeeting: Factory.dummyMeeting({location: roomWithMeeting, start: now.subtract(6, 'minute'), end: now.add(5, 'minute').add(1, 'second')})
-        });
-        td.when(mockCalendarService.getCurrentMeetings(roomEndingSoon)).thenResolve({
-            currentMeeting: Factory.dummyMeeting({location: roomEndingSoon, end: now.add(5, 'minute')})
-        });
-        td.when(mockCalendarService.getCurrentMeetings(roomEnded)).thenResolve({
-            currentMeeting: Factory.dummyMeeting({location: roomEnded, end: now.subtract(1, 'second')})
-        });
-        td.when(mockCalendarService.getCurrentMeetings(roomNoMeeting)).thenResolve({
-            previousMeeting: Factory.dummyMeeting({location: roomNoMeeting, end: now.subtract(2, 'minute').subtract(1, 'second')})
-        });
-        td.when(mockCalendarService.getCurrentMeetings(roomJustStarted)).thenResolve({
-            currentMeeting: Factory.dummyMeeting({location: roomJustStarted, start: now.subtract(1, 'second')})
-        });
+        test('meeting just started', async () => {
+            td.when(mockCalendarService.getCurrentMeetings(room)).thenResolve({
+                previousMeeting: Factory.dummyMeeting({end: now.subtract(3, 'm')}),
+                currentMeeting: Factory.dummyMeeting({start: now.subtract(1, 'm')})
+            })
+            await app.syncBulbs()
+            td.verify(bulb.pulse(Color.GREEN))
+        })
 
-        // Call test method
-        await app.syncBulbs()
+        test('previous meeting just ended, no current meeting', async () => {
+            td.when(mockCalendarService.getCurrentMeetings(room)).thenResolve({
+                previousMeeting: Factory.dummyMeeting({end: now.subtract(1, 'm')})
+            })
+            await app.syncBulbs()
+            td.verify(bulb.setColor(Color.RED))
+        })
 
-        // Verify results
-        td.verify(bulbWithMeeting.setColor(Color.GREEN_SOFT))
-        td.verify(bulbEndingSoon.setColor(Color.ORANGE))
-        td.verify(bulbEnded.setColor(Color.RED))
-        td.verify(bulbNoMeeting.powerOn(false))
-        td.verify(bulbJustStarted.setColor(Color.GREEN))
+        test('meeting just ended, next meeting', async () => {
+            td.when(mockCalendarService.getCurrentMeetings(room)).thenResolve({
+                currentMeeting: Factory.dummyMeeting({end: now}),
+                nextMeeting: Factory.dummyMeeting({start: now})
+            })
+            await app.syncBulbs()
+            td.verify(bulb.pulse(Color.RED))
+        })
+
+        test('previous meeting just ended, current meeting', async () => {
+            td.when(mockCalendarService.getCurrentMeetings(room)).thenResolve({
+                previousMeeting: Factory.dummyMeeting({end: now.subtract(1, 'm')}),
+                currentMeeting: Factory.dummyMeeting({start: now})
+            })
+            await app.syncBulbs()
+            td.verify(bulb.pulse(Color.RED))
+        })
+
+        test('meeting about to end', async () => {
+            td.when(mockCalendarService.getCurrentMeetings(room)).thenResolve({
+                currentMeeting: Factory.dummyMeeting({end: now.add(2, 'm')})
+            })
+            await app.syncBulbs()
+            td.verify(bulb.setColor(Color.ORANGE))
+        })
+
+        test('no meeting', async () => {
+            td.when(mockCalendarService.getCurrentMeetings(room)).thenResolve({})
+            await app.syncBulbs()
+            td.verify(bulb.powerOn(false))
+        })
 
     });
 
